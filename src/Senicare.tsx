5e144609d6fecd70be69aae4da55d9e4ce4f626a
imwkdrh <imwkdrh7@naver.com>
2024-08-21 13:41:41 +0900

    2024-08-21 13:41

---
+++ src/Senicare.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Senicare.css';
+import Auth from 'src/views/Auth';
+
+export default function Senicare() {
+  return (
+    <>
+      <Auth />
+    </>
+  );
+}
+
+
